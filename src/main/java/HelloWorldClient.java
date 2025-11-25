c6c015d6737c3a27e4d3c8e88c6f178e7ab3920e
mkovacevic10 <max.billroth@gmail.com>
2025-11-25 09:09:02 +0100

    Namen geändert

--- src/main/java/HelloWorldClient.java
+++ src/main/java/HelloWorldClient.java
@@ -5,8 +5,8 @@ public class HelloWorldClient {
 
     public static void main(String[] args) {
 
-        String firstname = args.length > 0 ? args[0] : "Max";
-        String lastname  = args.length > 1 ? args[1] : "Mustermann";
+        String firstname = args.length > 0 ? args[0] : "Maksym";
+        String lastname  = args.length > 1 ? args[1] : "Kovacevic";
 
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext()
